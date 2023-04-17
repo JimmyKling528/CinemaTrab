fc8474fd63222d54fcdfeefb972e964b57351033
JimmyKling528 <105024769+JimmyKling528@users.noreply.github.com>
2023-04-17 15:43:04 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,38 +1,39 @@
+@startuml
 
-
-class Cinema :
+class Cinema {
     - nome: string
     - endereco: string
     - salas: list<Sala>
+}
 
-
-class Sala :
+class Sala {
     - numero: int
     - capacidade: int
+}
 
-
-class Sessao :
+class Sessao {
     - filme: string
     - horario: datetime
     - sala: Sala
     - preco: float
     - ingressos_vendidos: int
     + comprar_ingresso(): Ingresso
+}
 
-
-class Ingresso :
+class Ingresso {
     - sessao: Sessao
     - preco: float
+}
 
-
-class Cliente :
+class Cliente {
     - nome: string
     - idade: int
     - email: string
-
+}
 
 Cinema *-- "salas" Sala
 Sala *-- Sessao
 Sessao *-- Ingresso
 Cliente *-- Ingresso
 
+@enduml
